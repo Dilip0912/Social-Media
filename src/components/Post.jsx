1f39548e9e9eb034ada9555edb7ef346dd9a3b49
Dilip <dilipk1261@gmail.com>
2025-02-13 14:33:57 +0530

    Delete Post Button

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,23 @@
+import { useContext } from "react";
+import { TiDelete } from "react-icons/ti";
+import { postListContext } from "../store/PostListContext";
+
 function Post({ post }) {
+  const {deletePost}=useContext(postListContext);
+
   return (
     <div class="card Post-card" style={{ width: "18rem" }}>
       <div class="card-body">
-        <h5 class="card-title">{post.title}</h5>
+        <h5 class="card-title">
+          {post.title}
+          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" onClick={()=>deletePost(post.id)}>
+            <TiDelete />
+          </span>
+        </h5>
         <p class="card-text">{post.body}</p>
-        {post.tags.map((tag)=><span className="badge text-bg-primary tag-span">{`#${tag}`}</span>)}
+        {post.tags.map((tag) => (
+          <span className="badge text-bg-primary tag-span">{`#${tag}`}</span>
+        ))}
       </div>
     </div>
   );
--- src/store/PostListContext.jsx
+++ src/store/PostListContext.jsx
@@ -7,15 +7,26 @@ export const postListContext = createContext({
 });
 
 const PostListReducer = (currentPostList, action) => {
-  return currentPostList;
+  let newPostList=currentPostList;
+  if(action.key==="Delete_Post"){
+    newPostList=currentPostList.filter(post=>post.id!==action.payload.postId);
+  }
+  return newPostList;
 };
 
 const PostListProvider = ({ children }) => {
-  const [postList, dipatchPostList] = useReducer(PostListReducer,Default_Post_List);
+  const [postList, dispatchPostList] = useReducer(PostListReducer,Default_Post_List);
 
   const addPost = () => {};
 
-  const deletePost = () => {};
+  const deletePost = (postId) => {
+    dispatchPostList({
+      key:"Delete_Post",
+      payload:{
+        postId,
+      }
+    })
+  };
 
   return (
     <postListContext.Provider value={{ postList, addPost, deletePost }}>
